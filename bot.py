60ae637621b6225895883ea60b27b52239e42fb7
marinanellina <marinanellina@me.com>
2026-06-01 20:36:16 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,16 @@
+import os
+
+from telegram import Update
+from telegram.ext import (
+    Application,
+    MessageHandler,
+    ContextTypes,
+    filters
+)
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+
 async def guard(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
         msg = update.message
@@ -5,7 +18,7 @@ async def guard(update: Update, context: ContextTypes.DEFAULT_TYPE):
         if not msg:
             return
 
-        # Сообщение канала или системное сообщение не трогаем
+        # Сообщение канала не трогаем
         if msg.sender_chat:
             return
 
@@ -13,8 +26,17 @@ async def guard(update: Update, context: ContextTypes.DEFAULT_TYPE):
         if msg.message_thread_id:
             return
 
-        # Обычное сообщение пользователя удаляем
+        # Обычное сообщение в группе удаляем
         await msg.delete()
 
     except Exception as e:
         print(e)
+
+
+app = Application.builder().token(TOKEN).build()
+
+app.add_handler(
+    MessageHandler(filters.ALL, guard)
+)
+
+app.run_polling()
