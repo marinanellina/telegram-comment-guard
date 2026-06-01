a9c54d98e4a7f4bbd27f3d114a9bccbff95e2c0e
marinanellina <marinanellina@me.com>
2026-06-01 19:48:12 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,32 @@
+import os
 from telegram import Update
 from telegram.ext import Application, MessageHandler, ContextTypes, filters
 
-import os
-
 TOKEN = os.getenv("BOT_TOKEN")
 
-async def debug(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+async def guard(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
-        print("=" * 80)
-        print(update.to_dict())
-        print("=" * 80)
+        msg = update.message
+
+        if not msg:
+            return
+
+        # Комментарий к посту канала
+        if msg.reply_to_message and msg.message_thread_id:
+            return
+
+        # Всё остальное удаляем
+        await msg.delete()
+
     except Exception as e:
         print(e)
+
 
 app = Application.builder().token(TOKEN).build()
 
 app.add_handler(
-    MessageHandler(filters.ALL, debug)
+    MessageHandler(filters.ALL, guard)
 )
 
 app.run_polling()
