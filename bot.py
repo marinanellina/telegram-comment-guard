4e472e05c5c6b64e8ff6fd44804e1ec395fce28a
marinanellina <marinanellina@me.com>
2026-06-01 20:53:38 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,25 +12,7 @@ TOKEN = os.getenv("BOT_TOKEN")
 
 
 async def guard(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    try:
-        msg = update.message
-
-        if not msg:
-            return
-
-        # Сообщение канала не трогаем
-        if msg.sender_chat:
-            return
-
-        # Комментарий к посту канала
-        if msg.message_thread_id:
-            return
-
-        # Обычное сообщение в группе удаляем
-        await msg.delete()
-
-    except Exception as e:
-        print(e)
+    print(update.to_dict())
 
 
 app = Application.builder().token(TOKEN).build()
