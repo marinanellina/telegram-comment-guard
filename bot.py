1c2cff1d1142da53a1e1538744113b90f766457a
marinanellina <marinanellina@me.com>
2026-06-01 22:42:49 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,29 @@
 import os
-from telegram.ext import Application
+
+from telegram import Update
+from telegram.ext import (
+    Application,
+    MessageHandler,
+    ContextTypes,
+    filters,
+)
 
 TOKEN = os.getenv("BOT_TOKEN")
-print("БОТ ЗАПУСТИЛСЯ")
+
+
+async def guard(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    msg = update.message
+
+    if not msg:
+        return
+
+    print(update.to_dict())
+
 
 app = Application.builder().token(TOKEN).build()
 
-print("BOT STARTED")
+app.add_handler(
+    MessageHandler(filters.ALL, guard)
+)
 
 app.run_polling()
